8b941cd44dbb0994cbd3c7527d594d7b469f5733
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-04 13:18:19 +0300

    1 - definim checkUnique in CategoryService
    2- CategoryRestController si definim checkUnique
    
    3- Am creat modal_fragments.html pt modal dialog

---
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/CategoryRestController.java
@@ -0,0 +1,26 @@
+package com.shopme.admin.controller;
+
+
+import com.shopme.admin.service.CategoryService;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryRestController {
+
+    private final CategoryService categoryService;
+
+    public CategoryRestController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/categories/check_unique")
+    public String checkUnique(@Param("id") Integer id,
+                              @Param("name") String name,
+                              @Param("alias") String alias) {
+
+        return categoryService.checkUnique(id, name, alias);
+    }
+
+}
