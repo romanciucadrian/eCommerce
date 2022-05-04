c39ba31297c7701a1d15750c923bc54a87ceaef4
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-04 12:54:14 +0300

    Organizare si merge totul bine

---
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/service/impl/ICategoryService.java
@@ -0,0 +1,17 @@
+package com.shopme.admin.service.impl;
+
+import java.util.List;
+
+import com.shopme.admin.error.CategoryNotFoundException;
+import com.shopme.common.entity.Category;
+
+public interface ICategoryService {
+
+    public List<Category> listAll();
+
+    public List<Category> listCategoriesUsedInForm();
+
+    public Category save(Category category);
+
+    public Category getID(Integer id) throws CategoryNotFoundException;
+}
