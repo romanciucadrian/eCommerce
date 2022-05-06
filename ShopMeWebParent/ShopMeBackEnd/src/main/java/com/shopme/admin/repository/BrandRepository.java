fd802425af2365796546bdd67c7bfd05ab2dc845
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-06 12:45:29 +0300

    Task 49: Create countById in BrandRepository

--- ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/repository/BrandRepository.java
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/repository/BrandRepository.java
@@ -4,4 +4,6 @@ import com.shopme.common.entity.Brand;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+
+    public Long countById(Integer id);
 }
