cdde395f6e4deb71cc7f8b14578f07ae1ae6bb67
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-06 12:50:02 +0300

    Task 50: Define save,get,delete function in BrandService

--- ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/service/BrandService.java
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/service/BrandService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.service;
 
+import com.shopme.admin.error.BrandNotFoundException;
 import com.shopme.admin.repository.BrandRepository;
 import com.shopme.admin.service.impl.IBrandService;
 import com.shopme.common.entity.Brand;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @Transactional
@@ -18,9 +20,34 @@ public class BrandService implements IBrandService {
         this.brandRepository = brandRepository;
     }
 
-
     @Override
     public List<Brand> listAll() {
         return (List<Brand>) brandRepository.findAll();
+    }
+
+    @Override
+    public Brand save(Brand brand) {
+        return brandRepository.save(brand);
+    }
+
+    @Override
+    public Brand get(Integer id) throws BrandNotFoundException {
+        try {
+            return brandRepository.findById(id).get();
+        } catch (NoSuchElementException ex) {
+            throw new BrandNotFoundException("Could not find any brand with ID" + id);
+        }
+    }
+
+    @Override
+    public void delete(Integer id) throws BrandNotFoundException {
+
+        Long countById = brandRepository.countById(id);
+
+        if (countById == null || countById == 0) {
+            throw new BrandNotFoundException("Could not find any brand with ID " + id);
+        }
+
+        brandRepository.deleteById(id);
     }
 }
