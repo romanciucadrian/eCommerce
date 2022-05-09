51a68c360b4b4d6cfe837a394346e07cdcebbaab
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-09 12:55:23 +0300

    Task 69: Define exposeDirectory function to delete duplicated code in MvcConfig.java

--- ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/config/MvcConfig.java
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/config/MvcConfig.java
@@ -13,34 +13,27 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-
         // User
-        String dirName = "user-photos";
-        Path userPhotosDir = Paths.get(dirName);
-
-        String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/" + dirName + "/**")
-                .addResourceLocations("file:/" + userPhotosPath + "/");
+        exposeDirectory("user-photos", registry);
 
         // Category
-        String categoryImagesDirName = "../category-images";
-        Path categoryImagesDir = Paths.get(categoryImagesDirName);
-
-        String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/category-images/**")
-                .addResourceLocations("file:/" + categoryImagesPath + "/");
-
+        exposeDirectory("../category-images", registry);
 
         // Brand
-        String brandLogosDirName = "../brand-logos";
-        Path brandLogosDir = Paths.get(brandLogosDirName);
+        exposeDirectory("../brand-logos", registry);
 
-        String brandLogosPath = brandLogosDir.toFile().getAbsolutePath();
+    }
 
-        registry.addResourceHandler("/brand-logos/**")
-                .addResourceLocations("file:/" + brandLogosPath + "/");
+    private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+
+        Path path = Paths.get(pathPattern);
+        String absolutePath = path.toFile().getAbsolutePath();
+
+        String logicalPath = pathPattern.replace("../", "") + "/**";
+
+        registry.addResourceHandler(logicalPath)
+                .addResourceLocations("file:/" + absolutePath + "/");
+
     }
 }
 
