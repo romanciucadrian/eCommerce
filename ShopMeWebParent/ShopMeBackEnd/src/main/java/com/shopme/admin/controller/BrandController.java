edaf4947690353e0c41bb01c7dcf13bf513a9ad0
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-09 10:09:27 +0300

    Task 64: Define listFirstPage and listByPage methods in BrandController.java

--- ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/BrandController.java
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/BrandController.java
@@ -7,6 +7,8 @@ import com.shopme.admin.service.CategoryService;
 import com.shopme.admin.util.FileUploadUtil;
 import com.shopme.common.entity.Brand;
 import com.shopme.common.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -33,10 +35,38 @@ public class BrandController {
 
 //<!-- BRANDS -->
     @GetMapping("/brands")
-    public String listAll(Model model) {
+    public String listFirstPage(Model model) {
 
-        List<Brand> listBrands = brandService.listAll();
+        return listByPage(1, model, "name", "asc", null);
+    }
 
+    @GetMapping("/brands/page/{pageNum}")
+    public String listByPage(@PathVariable(name = "pageNum") int pageNum,
+                             Model model,
+                             @Param("sortField") String sortField,
+                             @Param("sortDir") String sortDir,
+                             @Param("keyword") String keyword) {
+
+        Page<Brand> page = brandService.listByPage(pageNum, sortField, sortDir, keyword);
+
+        List<Brand> listBrands = page.getContent();
+
+        long startCount = (long) (pageNum - 1) * BrandService.BRANDS_PER_PAGE + 1;
+        long endCount = startCount + BrandService.BRANDS_PER_PAGE - 1;
+        if( endCount > page.getTotalElements()) {
+            endCount = page.getTotalElements();
+        }
+
+        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", reverseSortDir);
+        model.addAttribute("keyword", keyword);
         model.addAttribute("listBrands", listBrands);
 
         return "brands/brands";
