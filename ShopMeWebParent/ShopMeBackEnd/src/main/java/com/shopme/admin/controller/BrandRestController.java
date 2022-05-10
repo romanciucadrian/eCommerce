6bf401a4031e0108c473bd4d01a3229677ac047f
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-10 13:37:46 +0300

    Task 83: Define listCategoriesByBrand in BrandRestController

--- ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/BrandRestController.java
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/BrandRestController.java
@@ -1,9 +1,19 @@
 package com.shopme.admin.controller;
 
+import com.shopme.admin.error.BrandNotFoundException;
+import com.shopme.admin.error.BrandNotFoundRestException;
 import com.shopme.admin.service.BrandService;
+import com.shopme.admin.util.CategoryDTO;
+import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BrandRestController {
@@ -19,5 +29,24 @@ public class BrandRestController {
                                       @Param("name") String name) {
 
         return brandService.checkUnique(id, name);
+    }
+
+    @GetMapping("/brands/{id}/categories")
+    public List<CategoryDTO> listCategoriesByBrand(@PathVariable(name = "id") Integer id) throws BrandNotFoundRestException {
+
+        List<CategoryDTO> listCategories = new ArrayList<>();
+
+        try {
+            Brand brand = brandService.getCategories();
+
+            for (Category category : categories) {
+
+                CategoryDTO categoryDTO = new CategoryDTO(category.getId(), category.getName());
+                listCategories.add(categoryDTO);
+            }
+            return listCategories;
+        } catch (BrandNotFoundException e) {
+            throw new BrandNotFoundRestException();
+        }
     }
 }
