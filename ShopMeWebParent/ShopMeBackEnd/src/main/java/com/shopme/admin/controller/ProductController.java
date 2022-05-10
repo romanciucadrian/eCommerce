c65ea4e84b671e156ac65b984a45322d1be4fa12
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-10 12:57:16 +0300

    Task 77: Create ProductController and define listAll

---
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.shopme.admin.controller;
+
+import java.util.List;
+
+import com.shopme.admin.service.impl.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.admin.service.ProductService;
+import com.shopme.common.entity.Product;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String listAll(Model model) {
+        List<Product> listProducts = productService.listAll();
+
+        model.addAttribute("listProducts", listProducts);
+
+        return "products/products";
+    }
+}
