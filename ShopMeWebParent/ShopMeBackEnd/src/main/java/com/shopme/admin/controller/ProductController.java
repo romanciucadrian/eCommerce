1213d21eeb22aeb832c4a16b05c1904b5fdfd159
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-10 13:44:37 +0300

    Task 84:Define newProduct and saveProduct method and Add loggers in ProductController

--- ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
@@ -2,21 +2,26 @@ package com.shopme.admin.controller;
 
 import java.util.List;
 
+import com.shopme.admin.service.BrandService;
 import com.shopme.admin.service.ProductService;
+import com.shopme.common.entity.Brand;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 import com.shopme.common.entity.Product;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
 
     private final ProductService productService;
+    private final BrandService brandService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, BrandService brandService) {
         this.productService = productService;
+        this.brandService = brandService;
     }
 
     @GetMapping("/products")
@@ -26,5 +31,26 @@ public class ProductController {
         model.addAttribute("listProducts", listProducts);
 
         return "products/products";
+    }
+
+    @GetMapping("/products/new")
+    public String newProduct(Model model) {
+
+        List<Brand> listBrands = brandService.listAll();
+
+        Product product = new Product();
+        product.setEnabled(true);
+        product.setInStock(true);
+
+        model.addAttribute("product", product);
+        model.addAttribute("listBrands", listBrands);
+        model.addAttribute("pageTitle", "Create New Product");
+
+        return "products/product_form";
+    }
+
+    @PostMapping("/products/save")
+    public String saveProduct(Product product) {
+        return "redirect:/products";
     }
 }
