73be4793329459d683bde80277f87b03bac6d45f
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-11 14:36:53 +0300

    Task 120: Fix checkUniqueUrl issue after defining it into product_form.html and changing its url into product_form.js

--- ShopMeWebParent/ShopMeBackEnd/src/main/resources/static/js/product_form.js
+++ ShopMeWebParent/ShopMeBackEnd/src/main/resources/static/js/product_form.js
@@ -92,7 +92,7 @@ function checkUnique(form) {
 
     params = {id: productId, name: productName, _csrf: csrfValue};
 
-    $.post(url, params, function(response) {
+    $.post(checkUniqueUrl, params, function(response) {
         if (response == "OK") {
             form.submit();
         } else if (response == "Duplicate") {
