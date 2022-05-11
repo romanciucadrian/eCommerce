0b299a9369cd2ba2eff879751f24b2c6a1126c6d
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-11 12:40:41 +0300

    Task 108:  Define ProductImage process in Product

--- ShopMeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopMeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "products")
@@ -55,6 +57,9 @@ public class Product {
     private float height;
     private float weight;
 
+    @Column(name = "main_image", nullable = false)
+    private String mainImage;
+
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
@@ -62,6 +67,13 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "brand_id")
     private Brand brand;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private Set<ProductImage> images = new HashSet<>();
+
+    public void addExtraImage(String imageName) {
+        this.images.add(new ProductImage(imageName, this));
+    }
 
     @Override
     public String toString() {
