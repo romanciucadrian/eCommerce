0f76e97d02df5d99c62cb715340d1559d1b86cb6
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-11 13:42:22 +0300

    Task 119:  Add multi extra images feature in saveProduct and deleteProduct function of ProductController

--- ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
@@ -60,24 +60,18 @@ public class ProductController {
     @PostMapping("/products/save")
     public String saveProduct(Product product,
                               RedirectAttributes redirectAttributes,
-                              @RequestParam("fileImage")MultipartFile multipartFile) throws IOException {
+                              @RequestParam("fileImage")MultipartFile mainImageMultipart,
+                              @RequestParam("extraImage") MultipartFile[] extraImageMultiparts) throws IOException {
 
-        if (!multipartFile.isEmpty()) {
-            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        setMainImageName(mainImageMultipart, product);
 
-            product.setMainImage(fileName);
+        setExtraImageNames(extraImageMultiparts, product);
 
-            Product savedProduct = productService.save(product);
+        Product savedProduct = productService.save(product);
 
-            String uploadDir = "../product-images/" + savedProduct.getId();
+        saveUploadedImages(mainImageMultipart, extraImageMultiparts, savedProduct);
 
-            FileUploadUtil.cleanDir(uploadDir);
-            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-        } else {
-            productService.save(product);
-        }
-
-        redirectAttributes.addFlashAttribute("messageSuccess", "The product has been saved !");
+        redirectAttributes.addFlashAttribute("messageSuccess", "The product has been saved successfully.");
 
         return "redirect:/products";
     }
@@ -105,10 +99,70 @@ public class ProductController {
         try {
             productService.delete(id);
 
-            redirectAttributes.addFlashAttribute("message", "The product ID " + id + " has been deleted!");
+            String productExtraImagesDir = "../product-images/" + id + "/extras";
+            String productImagesDir = "../product-images/" + id;
+
+            FileUploadUtil.removeDir(productExtraImagesDir);
+            FileUploadUtil.removeDir(productImagesDir);
+
+            redirectAttributes.addFlashAttribute("messageSuccess", "The product ID " + id + " has been deleted!");
         } catch (ProductNotFoundException ex) {
-            redirectAttributes.addFlashAttribute("message", ex.getMessage());
+            redirectAttributes.addFlashAttribute("messageError", ex.getMessage());
         }
         return "redirect:/products";
+    }
+
+    private void setExtraImageNames(MultipartFile[] extraImageMultiparts, Product product) {
+
+        if (extraImageMultiparts.length > 0) {
+
+            for (MultipartFile multipartFile : extraImageMultiparts) {
+                if (!multipartFile.isEmpty()) {
+
+                    String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+                    product.addExtraImage(fileName);
+                }
+            }
+        }
+    }
+
+    private void setMainImageName(MultipartFile mainImageMultipart, Product product) {
+
+        if (!mainImageMultipart.isEmpty()) {
+
+            String fileName = StringUtils.cleanPath(mainImageMultipart.getOriginalFilename());
+
+            product.setMainImage(fileName);
+        }
+    }
+
+    private void saveUploadedImages(MultipartFile mainImageMultipart,
+                                    MultipartFile[] extraImageMultiparts, Product savedProduct) throws IOException {
+
+        if (!mainImageMultipart.isEmpty()) {
+            String fileName = StringUtils.cleanPath(mainImageMultipart.getOriginalFilename());
+
+            String uploadDir = "../product-images/" + savedProduct.getId();
+
+            FileUploadUtil.cleanDir(uploadDir);
+
+            FileUploadUtil.saveFile(uploadDir, fileName, mainImageMultipart);
+        }
+
+        if (extraImageMultiparts.length > 0) {
+
+            String uploadDir = "../product-images/" + savedProduct.getId() + "/extras";
+
+            for (MultipartFile multipartFile : extraImageMultiparts) {
+
+                if (multipartFile.isEmpty()) continue;
+
+                String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+                FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
+            }
+        }
     }
 }
