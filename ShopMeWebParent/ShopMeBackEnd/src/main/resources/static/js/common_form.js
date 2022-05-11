1a1fd8f2964927c2cffeb8a383a78c90446ddd6e
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-11 13:23:29 +0300

    Task 115: Define checkFileSize method in common_form.js

--- ShopMeWebParent/ShopMeBackEnd/src/main/resources/static/js/common_form.js
+++ ShopMeWebParent/ShopMeBackEnd/src/main/resources/static/js/common_form.js
@@ -7,25 +7,35 @@ $(document).ready(function() {
     });
 
     $("#fileImage").change(function() {
-        let fileSize = this.files[0].size;
-
-        if (fileSize > 1024000) {
-            this.setCustomValidity("You must choose an image less than 100KB!");
-            this.reportValidity();
-        } else {
-            this.setCustomValidity("");
-            showImageThumbnail(this);
+        if (!checkFileSize(this)) {
+            return;
         }
-
+        showImageThumbnail(this);
     });
 });
+
+function checkFileSize(fileInput) {
+    fileSize = fileInput.files[0].size;
+
+    if (fileSize > MAX_FILE_SIZE) {
+        fileInput.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE + " bytes!");
+        fileInput.reportValidity();
+
+        return false;
+    } else {
+        fileInput.setCustomValidity("");
+
+        return true;
+    }
+}
+
 
 function showImageThumbnail(fileInput) {
     let file = fileInput.files[0];
     let reader = new FileReader();
+
     reader.onload = function(e) {
         $("#thumbnail").attr("src", e.target.result);
     };
-
     reader.readAsDataURL(file);
 }
