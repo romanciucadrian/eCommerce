67a7fec38507099e8ed56faa65dd1beb02ae8a3d
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-11 15:21:45 +0300

    Task 123:  Add Product's Details option in saveProduct of ProductController || Delete product_form.js

--- ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
@@ -61,11 +61,15 @@ public class ProductController {
     public String saveProduct(Product product,
                               RedirectAttributes redirectAttributes,
                               @RequestParam("fileImage")MultipartFile mainImageMultipart,
-                              @RequestParam("extraImage") MultipartFile[] extraImageMultiparts) throws IOException {
+                              @RequestParam("extraImage") MultipartFile[] extraImageMultiparts,
+                              @RequestParam(name = "detailNames", required = false) String[] detailNames,
+                              @RequestParam(name = "detailValues", required = false) String[] detailValues) throws IOException {
 
         setMainImageName(mainImageMultipart, product);
 
         setExtraImageNames(extraImageMultiparts, product);
+
+        setProductDetails(detailNames, detailValues, product);
 
         Product savedProduct = productService.save(product);
 
@@ -162,6 +166,20 @@ public class ProductController {
 
                 FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 
+            }
+        }
+    }
+
+    private void setProductDetails(String[] detailNames, String[] detailValues, Product product) {
+
+        if (detailNames == null || detailNames.length == 0) return;
+
+        for (int count = 0; count < detailNames.length; count++) {
+            String name = detailNames[count];
+            String value = detailValues[count];
+
+            if (!name.isEmpty() && !value.isEmpty()) {
+                product.addDetail(name, value);
             }
         }
     }
