0f78683397bba759ce3c8421b5ee4c2193e31a0c
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-11 16:17:10 +0300

    Task 131: Define edit process in Product's Detail Form with adding delete option of each product

--- ShopMeWebParent/ShopMeBackEnd/src/main/resources/static/js/product_form_details.js
+++ ShopMeWebParent/ShopMeBackEnd/src/main/resources/static/js/product_form_details.js
@@ -1,7 +1,16 @@
-function addNextDetailSection() {
-    let allDivDetails = $("[id^='divDetail']"); /** first id="divDetail0" defined in product_details.html then id="divDetail1" id="divDetail2" ... */
-    let divDetailsCount = allDivDetails.length;
+$(document).ready(function() {
+    $("a[name='linkRemoveDetail']").each(function(index) {
+        $(this).click(function() {
+            removeDetailSectionByIndex(index);
+        });
+    });
 
+});
+
+
+function addNextDetailSection() {
+    allDivDetails = $("[id^='divDetail']"); /** first id="divDetail0" defined in product_details.html then id="divDetail1" id="divDetail2" ... */
+    divDetailsCount = allDivDetails.length;
     htmlDetailSection = `
 		<div class="form-inline" id="divDetail${divDetailsCount}">
 			<label class="m-3">Name:</label>
@@ -10,25 +19,24 @@ function addNextDetailSection() {
 			<input type="text" class="form-control w-25" name="detailValues" maxlength="255" />
 		</div>	
 	`;
-
     $("#divProductDetails").append(htmlDetailSection);
-
-    let previousDivDetailSection = allDivDetails.last(); // get first previous value of the last element -> For instance : there are 2 elements,
+    previousDivDetailSection = allDivDetails.last(); // get first previous value of the last element -> For instance : there are 2 elements,
     // it only shows the icon at first one, like 3 elements , it shows the icon at 2th element.
 
-    let previousDivDetailID = previousDivDetailSection.attr("id");
-
+    previousDivDetailID = previousDivDetailSection.attr("id");
     htmlLinkRemove = `
 		<a class="btn fas fa-times-circle fa-2x icon-dark"
 			href="javascript:removeDetailSectionById('${previousDivDetailID}')"
 			title="Remove this detail"></a>
 	`;
-
     previousDivDetailSection.append(htmlLinkRemove);
-
     $("input[name='detailNames']").last().focus();
 }
 
 function removeDetailSectionById(id) {
     $("#" + id).remove();
+}
+
+function removeDetailSectionByIndex(index) {
+    $("#divDetail" + index).remove();
 }
