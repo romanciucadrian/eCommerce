a31d645dd60be1dac51c5b86dcf55c528af0b2ef
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-10-04 17:22:05 +0300

    Task 162:  Code controller layer for country

--- ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/CountryRestController.java
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/shopme/admin/controller/CountryRestController.java
@@ -2,10 +2,7 @@ package com.shopme.admin.controller;
 
 import com.shopme.admin.repository.CountryRepository;
 import com.shopme.common.entity.Country;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,10 @@ public class CountryRestController {
         Country savedCountry = countryRepository.save(country);
 
         return String.valueOf(savedCountry.getId());
+    }
+
+    @GetMapping("/countries/delete/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        countryRepository.deleteById(id);
     }
 }
